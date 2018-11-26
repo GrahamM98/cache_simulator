763c3e6f5638121b79be3a22904376f59740841a
GrahamM98 <michael@thegrahamfam.com>
2018-11-26 10:38:05 -0600

    Created structure for cache and does initial placement

--- src/cacheSim.py
+++ src/cacheSim.py
@@ -2,11 +2,11 @@ import sys
 import math
 
 #cache size in kilobytes
-cacheSize = 1024
+cacheSize = 1
 #cache line size in bytes
-cacheLineSize = 256
+cacheLineSize = 64
 #number of ways per set
-ways = 16
+ways = 4
 
 #calculates number of bits for offset
 offsetN = int(math.log(cacheLineSize, 2))
@@ -17,6 +17,13 @@ sets = int((1024*cacheSize)/(cacheLineSize*ways))
 #calculate number of bits for set index
 indexN = int(math.log(sets, 2))
 
+#set up cache structure
+cache = [None]*sets
+for i in range(sets):
+    cache[i] = [None]*ways
+    for j in range(ways):
+        cache[i][j] = {"tag": None, "val": 0, "data": None, "history": 0}
+
 def assign(addr):
     print(addr, end=', ')
     binAddr = bin(int(addr, 0))
@@ -26,17 +33,24 @@ def assign(addr):
     #find offset of 
     offset = '0b' + binAddr[-offsetN:]
     if offset == '0b': offset = '0b0'
-    print("offset: {0}".format(offset))
+    print("offset: {0}".format(int(offset, 0)))
 
     #find set index of current address
     index = '0b' + binAddr[-(offsetN+indexN):-offsetN]
     if index == '0b': index = '0b0'
-    print("set index: {0}".format(index))
+    print("set index: {0}".format(int(index, 0)))
 
     #find tag of current address
     tag = '0b' + binAddr[:-(offsetN+indexN)]
     if tag == '0b': tag = '0b0'
-    print("tag: {0}".format(tag))
+    print("tag: {0}".format(int(tag, 0)))
+
+    for j in range(ways):
+        if cache[int(index, 0)][j]["val"] == 0:
+            cache[int(index, 0)][j]["tag"] = hex(int(tag, 0))
+            cache[int(index, 0)][j]["val"] = 1
+            cache[int(index, 0)][j]["data"] = str(cacheLineSize) + "b@" +addr
+            break
 
     print()
     return
@@ -48,4 +62,9 @@ print("address space: {0}, sets: {1}, set index: {2}, offset: {3}".format(addrSi
 for line in file:
     addr = line.split()[-1]
     assign(addr)
+for i in range(sets):
+    print("set: {0}".format(i))
+    for j in range(ways):
+        print("{0}, {1}, {2}, {3}".format(cache[i][j]["tag"], cache[i][j]["val"], cache[i][j]["data"], cache[i][j]["history"]))
+    print()
     
