57c9faba79d714c2e86bd79deb7711873b32e6d0
BRiNK77 <31313958+BRiNK77@users.noreply.github.com>
2018-11-30 12:37:28 -0600

    Added LRU policy
    for loop insterted before updateHistory in the assign() func

--- src/cacheSim.py
+++ src/cacheSim.py
@@ -36,17 +36,20 @@ def updateHistory():
 def assign(addr):
     global missTotal
     global requestTotal
+
     print(addr, end=', ')
     binAddr = bin(int(addr, 0))
     print(binAddr)
+
+    # goes to second character in input line of address
     binAddr = binAddr[2:]
 
-    #find offset of 
+    # find offset of address
     offset = '0b' + binAddr[-offsetN:]
     if offset == '0b': offset = '0b0'
     print("offset: {0}".format(int(offset, 0)))
 
-    #find set index of current address
+    # find set index of current address
     index = '0b' + binAddr[-(offsetN+indexN):-offsetN]
     if index == '0b': index = '0b0'
     print("set index: {0}".format(int(index, 0)))
@@ -55,14 +58,15 @@ def assign(addr):
     tag = '0b' + binAddr[:-(offsetN+indexN)]
     if tag == '0b': tag = '0b0'
     print("tag: {0}".format(int(tag, 0)))
-    
+
     cacheMiss = 1
-    
+
     for j in range(ways):
         if cache[int(index, 0)][j]["tag"] == hex(int(tag, 0)):
             cacheMiss = 0
             break
 
+ # checks if miss due to empty way
     if cacheMiss == 1:
         for j in range(ways):
             if cache[int(index, 0)][j]["val"] == 0:
@@ -75,6 +79,22 @@ def assign(addr):
     if cacheMiss == 1:
         missTotal += 1
     requestTotal += 1
+
+
+
+    # for loop for replacement policy LRU
+    if cacheMiss == 1:
+        target = 0
+        maxHist = 0
+        for j in range(ways):
+            # gets max history reference in memory
+            if cache[int(index, 0)][j]["history"] >= maxHist
+                maxHist = cache[int(index, 0)][j]["history"]
+                target = j
+        cache[int(index, 0)][target]["tag"] = hex(int(tag, 0))
+        cache[int(index, 0)][target]["val"] = 1
+        cache[int(index, 0)][target]["data"] = str(cacheLineSize) + "b@" +addr
+        cache[int(index, 0)][target]["history"] = 0
 
     updateHistory()
 
@@ -94,4 +114,3 @@ for i in range(sets):
         print("{0}, {1}, {2}, {3}".format(cache[i][j]["tag"], cache[i][j]["val"], cache[i][j]["data"], cache[i][j]["history"]))
     print()
 print("Miss Rate: {0}%".format((float(missTotal)/float(requestTotal))*100))
-    
